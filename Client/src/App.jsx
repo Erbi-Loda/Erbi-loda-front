3b02aa49bfd6c3cd5025e4099aff655c40c7c779
alexis coronel <80395647+SacZ16@users.noreply.github.com>
2023-02-12 22:13:30 -0300

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AcercaDe from "./Components/Componentes_footer/AcercaDe/AcercaDe";
-import Ayuda from "./Components/Componentes_footer/Ayuda/Ayuda";
-import MiCuenta from "./Components/Componentes_footer/MiCuenta/MiCuenta";
-import RedesSociales from "./Components/Componentes_footer/RedesSociales/RedesSociales";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 import PostProduct from "./Components/PostProduct/PostProduct";
@@ -16,10 +12,6 @@ function App() {
          <Route path="/post" element={<PostProduct />} /> 
          <Route path="/login" element={<Login/>} /> 
         <Route path="/register" element={<Register/>} />
-        <Route path="/infoAyuda" element={<Ayuda/>} />
-        <Route path="/acercaDe" element={<AcercaDe/>} />
-        <Route path="/redesSociales" element={<RedesSociales/>} />
-        <Route path="/miCuenta" element={<MiCuenta/>} />
       </Routes>
     </BrowserRouter>
   );
