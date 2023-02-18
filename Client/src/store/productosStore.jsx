3ff484ae90d152844e4dc1997634dab6c05aed81
alexis coronel <80395647+SacZ16@users.noreply.github.com>
2023-02-17 23:38:18 -0300

    Update productosStore.jsx

--- Client/src/store/productosStore.jsx
+++ Client/src/store/productosStore.jsx
@@ -5,7 +5,12 @@ import { create } from "zustand";
 export const useProductsStore = create((set, get) => ({
   product: false,
   getProducts: async () => {
-    const options = { method: "GET" };
+    const options = { method: "GET",headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Origin': '',
+      'authorization':'algo'
+    } };
     await fetch("http://localhost:8080/getProductos", options)
       .then((response) => response.json())
       .then((response) => set((state) => ({ ...state, product: response })))
