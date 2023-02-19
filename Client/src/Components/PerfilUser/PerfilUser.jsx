5f2ee6f30beb0f2082ac453a9d294f432610aa24
Agustin Kowalczuk <75805313+AgustinKowalczuk@users.noreply.github.com>
2023-02-18 23:56:19 -0300

    add perfil user

---
+++ Client/src/Components/PerfilUser/PerfilUser.jsx
@@ -0,0 +1,37 @@
+function PerfilUser(params) {
+  const infoUser = params || {
+    username: "Jorguito",
+    password: "jorge123",
+    email: "JorguitoVcp123@gmail.com",
+    compras: [{
+        nameProduct:"producto",
+        urlImg:'https://pixabay.com/es/photos/granos-de-caf%c3%a9-caf%c3%a9-asado-frijoles-917613/',
+        idProducto:1,
+        descriptionShort:"Lorem aoishdka sdi usadpifnalsd fhpiasn ekfjn aps udhfasen fpausi he;fonaspoefh a;sefpiaoseh f;oiasje fpoans df npoasfehf"
+    },{
+        nameProduct:"producto",
+        urlImg:'https://pixabay.com/es/photos/granos-de-caf%c3%a9-caf%c3%a9-asado-frijoles-917613/',
+        idProducto:2,
+        descriptionShort:"Lorem aoishdka sdi usadpifnalsd fhpiasn ekfjn aps udhfasen fpausi he;fonaspoefh a;sefpiaoseh f;oiasje fpoans df npoasfehf"
+    },{
+        nameProduct:"producto",
+        urlImg:'https://pixabay.com/es/photos/granos-de-caf%c3%a9-caf%c3%a9-asado-frijoles-917613/',
+        idProducto:3,
+        descriptionShort:"Lorem aoishdka sdi usadpifnalsd fhpiasn ekfjn aps udhfasen fpausi he;fonaspoefh a;sefpiaoseh f;oiasje fpoans df npoasfehf"
+    },
+    {
+        nameProduct:"producto",
+        urlImg:'https://pixabay.com/es/photos/granos-de-caf%c3%a9-caf%c3%a9-asado-frijoles-917613/',
+        idProducto:3,
+        descriptionShort:"Lorem aoishdka sdi usadpifnalsd fhpiasn ekfjn aps udhfasen fpausi he;fonaspoefh a;sefpiaoseh f;oiasje fpoans df npoasfehf"
+    },
+    {
+        nameProduct:"producto",
+        urlImg:'https://pixabay.com/es/photos/granos-de-caf%c3%a9-caf%c3%a9-asado-frijoles-917613/',
+        idProducto:3,
+        descriptionShort:"Lorem aoishdka sdi usadpifnalsd fhpiasn ekfjn aps udhfasen fpausi he;fonaspoefh a;sefpiaoseh f;oiasje fpoans df npoasfehf"
+    },],
+        companies: [{}],
+    historial: [{}]
+  };
+}
