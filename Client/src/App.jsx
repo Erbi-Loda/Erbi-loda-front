d80c781794d4d3cb30ea91ed871e3f772d729dc1
Agustin Kowalczuk <75805313+AgustinKowalczuk@users.noreply.github.com>
2023-02-22 23:20:08 -0300

    new folder and update tech icons

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,22 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { lazy, Suspense } from "react";
-// import Detalle from "./Components/Detalle/Detalle";
-// import Home from "./Components/Home/Home";
-// import Login from "./Components/Login/Login";
-// import PostProduct from "./Components/PostProduct/PostProduct";
-// import Register from "./Components/Register/Register";
-// import PanelUser from "./Components/PanelUser/PanelUser";
 const PanelUser=lazy(()=>import('./Components/PanelUser/PanelUser'))
 const Register=lazy(()=>import('./Components/Register/Register'))
 const Login=lazy(()=>import('./Components/Login/Login'))
 const Home=lazy(()=>import('./Components/Home/Home'))
 const Detalle =lazy(()=>import('./Components/Detalle/Detalle'))
 const PostProduct =lazy(()=>import('./Components/PostProduct/PostProduct'))
+const Loading =lazy(()=>import('./Components/Loading2/Loading'))
 
 function App() {
   return (
     <BrowserRouter>
-    <Suspense fallback={<div>Loading...</div>}>      
+    <Suspense fallback={<div className="loading"><Loading/></div>}>      
       <Routes>
         <Route path="*" element={<Navigate replace to ='/'/>} />
         <Route path="/" element={<Home />} />
--- Client/src/Components/Loading2/Loading.jsx
+++ Client/src/Components/Loading2/Loading.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-
 // estilos
-import './loadin.css'
+import "./loadin.css";
 
 export default function Loading() {
   return (
-    <div class="loader"></div>
-
-
+    <div className="container-Loader">
+      <div className="loader"></div>
+    </div>
   );
 }
