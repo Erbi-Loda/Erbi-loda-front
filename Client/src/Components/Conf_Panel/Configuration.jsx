996642a43640e8b6ff35534d0d219b474d93ef5d
Agustin Kowalczuk <75805313+AgustinKowalczuk@users.noreply.github.com>
2023-03-22 01:24:27 -0300

    modified in PanelUser

--- Client/src/Components/Conf_Panel/Configuration.jsx
+++ Client/src/Components/Conf_Panel/Configuration.jsx
@@ -1,21 +1,25 @@
+import { useEffect, useState } from "react";
 import "./Configuration.style.css";
 
 export default function Conf_Panel() {
+  const [muestra, setMuestra] = useState("Tema");
+
+  useEffect(()=>{
+    // alert(muestra)
+  },[muestra])
+
   return (
     <div className="box-setting">
-      <div className="settings">
-        <div className=" name-setting">
-          <h2 className="title-setting">Modificar nombre</h2>
-          {/* <form>
-            <input type="text" placeholder="Nuevo Nombre" />
-            <button></button>
-          </form> */}
-        </div>
-        <h2 className="title-setting">Modificar contraseña</h2>
-        <h2 className="title-setting">Modificar correo</h2>
-        <h2 className="title-setting">Modificar Tema</h2> {/* Dark/Original */}
-        {/* Pass Name Email Tema*/}
-      </div>
+      <ul className="settings">
+        <li><button className="button-setting" onClick={(e)=>{setMuestra("Name")}}>Modificar nombre</button></li>
+        <li><button className="button-setting" onClick={(e)=>{setMuestra("Pass")}}>Modificar contraseña</button></li>
+        <li><button className="button-setting" onClick={(e)=>{setMuestra("Mail")}}>Modificar correo</button></li>
+        <li><button className="button-setting" onClick={(e)=>{setMuestra("Tema")}}>Modificar Tema</button></li> {/* Dark/Original */}
+      </ul>
+      <section className="show-setting">
+        {/* aqui van a ir los componentes que debemos mostrar al usuario. */}
+        {muestra=="Name"?<h1>Hola Usuario, quieres cambiar tu nombre?</h1>:""}
+      </section>
     </div>
   );
 }
